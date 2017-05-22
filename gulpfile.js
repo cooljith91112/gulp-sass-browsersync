1279dab54594a098241279d9095dce4cfa0e096f
Indrajith K L <mac91112@gmail.com>
2017-05-22 15:35:19 +0530

    Added SCSS bundling

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
-var config={
+var config = {
   serverPath: './',
   sassSource: './sass',
-  cssDestination: './css'
-}
+  cssDestination: './css',
+  bundleFileName : 'bundle.css'
+};
 
 gulp.task('serve', ['sass'], function() {
 
@@ -16,14 +18,18 @@ gulp.task('serve', ['sass'], function() {
         server: config.serverPath
     });
 
-    gulp.watch(config.sassSource+"/**/*.scss", ['sass']);
-    gulp.watch("./**/*.html").on('change', browserSync.reload);
+    gulp.watch(config.sassSource+"/**/*.scss",{cwd:'./'},['sass']);
+    gulp.watch("./**/*.html",{cwd:'./'}).on('change', browserSync.reload);
+
+    browserSync.watch()
+
 });
 
 
 gulp.task('sass', function () {
   return gulp.src(config.sassSource+'/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(concat(config.bundleFileName))
+    .pipe(sass.sync().on('error', sass.logError))    
     .pipe(gulp.dest(config.cssDestination))
     .pipe(browserSync.stream({match: '**/*.css'}));
 });
